9c06e7f82608ab8b9261132c9ab07c2038ec8bd5
softwaresstar-hub <softwaresstar@gmail.com>
2026-02-11 21:16:23 +0530

    Create StudentModel.php file

---
+++ app/Models/StudentModel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class StudentModel extends Model
+{
+    protected $table = 'students';
+    protected $primaryKey = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType = 'array';
+    protected $allowedFields = ['name', 'email', 'phone', 'address', 'date_of_birth', 'class_id', 'enrollment_date', 'created_at', 'updated_at'];
+    protected $useTimestamps = true;
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
+}
